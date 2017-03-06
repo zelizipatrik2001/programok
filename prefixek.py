854d3e5361337ee2a9f4aec806a65b9842685d4e
zelizipatrik2001 <e16.zelizi.patrik@palfy-vizugyi.hu>
2017-03-06 12:56:33 +0100

    Add files via upload

---
+++ prefixek.py
@@ -0,0 +1,25 @@
+def prefix(a):
+    if a == "piko":
+        return("0.000000000001")
+    elif a == "nano":
+        return("0.000000001")
+    elif a == "mikro":
+        return("0.000001")
+    elif a == "mili":
+        return("0.001")
+    elif a == "centi":
+        return("0.01")
+    elif a == "deci":
+        return("0.1")
+    elif a == "deka":
+        return("10")
+    elif a == "hekto":
+        return("100")
+    elif a == "kilo":
+        return("1000")
+    elif a == "mega":
+        return("1000000")
+    elif a == "giga":
+        return("1000000000")
+    elif a == "terra":
+        return("1000000000000")
